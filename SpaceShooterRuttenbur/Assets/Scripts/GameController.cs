965f25dbbcc9e7f8873f07c3aff898939c5c70cd
Elizabeth Ruttenbur <elizabethruttenbur@gmail.com>
2018-01-21 21:02:42 -0700

    Updating Shooter

---
+++ SpaceShooterRuttenbur/Assets/Scripts/GameController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// The Game Controller for Space Shooter
+/// Liz "Frankie" Ruttenbur
+/// </summary>
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text restartText;
+    public Text gameOverText;
+    public Text scoreText;
+    private bool gameOver;
+    private bool restart;
+
+    private int score;
+    /// <summary>
+    /// updates and gets things going for the start of the game
+    /// </summary>
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        StartCoroutine(SpawnWaves()); //calls the spawn waves
+        
+    }
+    /// <summary>
+    /// the update function
+    /// that houses the restart ability for the game
+    /// </summary>
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Spawns the waves of hazards
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Adds the score together
+    /// and updates it
+    /// </summary>
+    /// <param name="newScoreValue"></param>
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    /// <summary>
+    /// Updates the score text
+    /// </summary>
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+    /// <summary>
+    /// updates the game over text
+    /// and sets the bool value to true 
+    /// breaking through the endless cycle.
+    /// </summary>
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
---
+++ SpaceShooterRuttenbur/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// New public boundary class
+/// that holds values for position for boundaries
+/// </summary>
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+/// <summary>
+/// Player Controller to handle player movement and firing for
+/// the space shooter game
+/// Liz "Frankie" Ruttenbur
+/// </summary>
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 100; //speed for the movement of player
+    public Boundary boundary; //the boundary class so you can use the boundary variables in this class
+    public float tilt; //the tilt for the player movement
+    private Rigidbody rb; //rigid body component
+    public GameObject shot; //the shot
+    public Transform shotSpawn; //the transform position of the shot
+    public float fireRate;
+    private float nextFire;
+
+    /// <summary>
+    /// start gets the rigidbody component and sets it to rb
+    /// </summary>
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+    /// <summary>
+    /// the update function sets the fire rate and the spawn shot stuff
+    /// plays the audio
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+    /// <summary>
+    /// Update is called once per frame
+    ///handles the physics
+    ///</summary>
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); //the movement captures
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.velocity = movement * speed;
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+
+   
+}

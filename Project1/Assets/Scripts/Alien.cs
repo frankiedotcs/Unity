ad857c82f214d5c00afbfb7739e7a7d1027c101f
Elizabeth Ruttenbur <elizabethruttenbur@gmail.com>
2018-01-31 20:53:00 -0700

    Finished and Updated
    Finished Project1-2

---
+++ Project1/Assets/Scripts/Alien.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alien : MonoBehaviour {
+
+    /// <summary>
+    /// public variables
+    /// </summary>
+    /// 
+    public Transform target;
+    public float navigationUpdate;
+
+    /// <summary>
+    /// private variables
+    /// </summary>
+    private NavMeshAgent agent;
+    private float navigationTime = 0;
+	
+    // Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = target.position;
+
+        if (navigationTime > navigationUpdate)
+        {
+            agent.destination = target.position;
+            navigationTime = 0;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+    }
+}

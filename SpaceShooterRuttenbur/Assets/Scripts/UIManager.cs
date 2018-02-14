a4095720bc002f8331587e76e2541a73c18451c9
Elizabeth Ruttenbur <elizabethruttenbur@gmail.com>
2018-02-13 23:19:51 -0700

    Spaceshooter Updated
    Adding enemies and background and a menu

---
+++ SpaceShooterRuttenbur/Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+    /// <summary>
+    /// public variables
+    /// </summary>
+    /// 
+    public Animator settingsButton;
+
+    public void OpenSettings()
+    {
+        settingsButton.SetBool("SettingsPressed", true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("spaceshooter");
+    }
+    
+	public void QuitGame()
+    {
+        //Application.Quit();
+        //use below for testing!
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}

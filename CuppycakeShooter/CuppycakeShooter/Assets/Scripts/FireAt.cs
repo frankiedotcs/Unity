24e7c81e3560f6850ce9f8f83250c3219ca81e08
Elizabeth Ruttenbur <elizabethruttenbur@gmail.com>
2018-03-01 16:03:42 -0700

    Firing bullets
    Firing cakes and rubber balls

---
+++ CuppycakeShooter/CuppycakeShooter/Assets/Scripts/FireAt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAt : MonoBehaviour {
+    public Rigidbody cake;
+    public Rigidbody bullet;
+    public float force = 10.0f;
+    public ForceMode forceMode;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody instance = Instantiate(cake, transform.position, transform.rotation) as Rigidbody;
+            instance.AddForce(transform.forward * force, forceMode);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Rigidbody instance = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+            instance.AddForce(transform.forward * force, forceMode);
+        }
+	}
+}
---
+++ CuppycakeShooter/CuppycakeShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 100; //speed for the movement of player
+    public float tilt; //the tilt for the player movement
+    private Rigidbody rb; //rigid body component
+    public GameObject shot; //the shot
+    public Transform shotSpawn; //the transform position of the shot
+
+    public float fireRate;
+    private float nextFire;
+    public bool rapidFire = false;
+    public float fastFire;
+    private float upgradeTime = 10f;
+    public int upgradesLeft = 2;
+    public bool quickMove;
+    private float currentRapidFireTime = 0.0f;
+    private float holdPress;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime *speed; //the movement captures
+        float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Rotate(0, moveHorizontal, 0);
+        transform.Translate(0, 0, moveVertical);
+    }
+
+   
+}
